49e20af436405cc8980b58867c1df8fb1289d071
saraalves <saraalves14@hotmail.com>
2024-09-01 12:40:59 -0300

    adicionando bottom navigation

--- app/src/main/java/com/saraalves/rickandmorty/presentation/RickAndMortyActivity.kt
+++ app/src/main/java/com/saraalves/rickandmorty/presentation/RickAndMortyActivity.kt
@@ -2,9 +2,9 @@ package com.saraalves.rickandmorty.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.saraalves.rickandmorty.R
 import com.saraalves.rickandmorty.databinding.ActivityRickAndMortyBinding
-import com.saraalves.rickandmorty.presentation.character.ViewPagerAdapter
 import com.saraalves.rickandmorty.presentation.character.fragment.CharacterFragment
 import com.saraalves.rickandmorty.presentation.episodes.EpisodesFragment
 import com.saraalves.rickandmorty.presentation.location.LocationFragment
@@ -13,48 +13,33 @@ class RickAndMortyActivity : AppCompatActivity(R.layout.activity_rick_and_morty)
 
     private val binding by lazy { ActivityRickAndMortyBinding.inflate(layoutInflater) }
 
-    private val tabLayout by lazy { binding.tabLayout }
-
     private lateinit var characterFragment: CharacterFragment
     private lateinit var episodeFragment: EpisodesFragment
     private lateinit var locationFragment: LocationFragment
-
-
-    private val _tabTitle = listOf(
-        "Personagens",
-        "Episódios",
-        "Localização",
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        configViewPager()
-        setupTabIcons()
-    }
-
-    private fun configViewPager() {
-
-        tabLayout.setupWithViewPager(binding.viewPager)
-
         characterFragment = CharacterFragment()
-        episodeFragment = EpisodesFragment()
         locationFragment = LocationFragment()
+        episodeFragment = EpisodesFragment()
 
-        val listaFragmentos = listOf(
-            characterFragment,
-            episodeFragment,
-            locationFragment
-        )
+        setCurrentFragment(characterFragment)
 
-        binding.viewPager.adapter = ViewPagerAdapter(listaFragmentos, supportFragmentManager)
-
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.person  -> setCurrentFragment(characterFragment)
+                R.id.episodes -> setCurrentFragment(episodeFragment)
+                R.id.location -> setCurrentFragment(locationFragment)
+            }
+            true
+        }
     }
 
-    private fun setupTabIcons() {
-        tabLayout.getTabAt(0)?.text = _tabTitle[0]
-        tabLayout.getTabAt(1)?.text = _tabTitle[1]
-        tabLayout.getTabAt(2)?.text = _tabTitle[2]
-    }
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
 }
